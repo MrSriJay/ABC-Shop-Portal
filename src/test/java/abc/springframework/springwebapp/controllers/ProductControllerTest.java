16f0c16cd2bb8a29ca9581e086209c723640703c
MrSriJay <jayanga.sl@gmail.com>
2021-07-22 09:27:12 +0530

    Add product method Unit test Added

--- src/test/java/abc/springframework/springwebapp/controllers/ProductControllerTest.java
+++ src/test/java/abc/springframework/springwebapp/controllers/ProductControllerTest.java
@@ -1,14 +1,20 @@
 package abc.springframework.springwebapp.controllers;
 
+import abc.springframework.springwebapp.domain.Product;
+import abc.springframework.springwebapp.domain.Promotion;
 import abc.springframework.springwebapp.repositories.ProductRepsitory;
 import abc.springframework.springwebapp.repositories.PromotionRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DataJpaTest
 class ProductControllerTest {
 
-
+    @Autowired
+    private ProductRepsitory productRepsitory;
 
     @Test
     void getProducts() {
@@ -22,6 +28,10 @@ class ProductControllerTest {
 
     @Test
     void createProduct() {
+
+        Product product = new Product("Snak Milk",180.00,"2022-01-04",0);
+        productRepsitory.save(product);
+
     }
 
     @Test
